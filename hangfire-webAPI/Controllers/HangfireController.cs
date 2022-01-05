827b33c769e0c0703e40623afaa6df7d6bce9042
JamesSloan <jamessloan94@gmail.com>
2022-01-05 19:12:25 +1100

    Hangfire implementation - Fire and forget, Delayed and Repeated jobs

---
+++ hangfire-webAPI/Controllers/HangfireController.cs
@@ -0,0 +1,53 @@
+using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hangfire_webAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HangfireController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from hangfire web api");
+        }
+
+        //Fire and Forget job example
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Welcome()
+        {
+            var jobId = BackgroundJob.Enqueue(() => SendWelcomeEmail("Welcome to our app"));
+
+            return Ok($"Job ID: {jobId}, Welcome email sent to the user");
+        }
+
+        //Delayed job example
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Discount()
+        {
+            var delay = 120;
+            var jobId = BackgroundJob.Schedule(() => SendWelcomeEmail("Welcome to our app"), TimeSpan.FromSeconds(delay));
+
+            return Ok($"Job ID: {jobId}, Discount email will be sent in {delay} seconds to the user");
+        }
+
+        //Repeated job example
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult DatabaseUpdate()
+        {
+            var frequency = 1;
+            RecurringJob.AddOrUpdate(() => Console.WriteLine("Database updated"), Cron.MinuteInterval(frequency));
+
+            return Ok("Database update job initiated");
+        }
+
+        public void SendWelcomeEmail(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ hangfire-webAPI/Program.cs
@@ -0,0 +1,29 @@
+using Hangfire;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddHangfire(x => x.UseSqlServerStorage(@"Data Source=JIMMY-8;Initial Catalog=hangfire-webAPI-db;Integrated Security=True;Pooling=False"));
+builder.Services.AddHangfireServer();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseHangfireDashboard();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
